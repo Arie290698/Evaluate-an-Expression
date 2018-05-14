e729bfd4b6b7531ad68376f4f2dd36131132f38a
zihadubaddika1004 <38626368+zihadubaddika1004@users.noreply.github.com>
2018-05-15 02:40:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,3 +18,21 @@ bool OperatorFound(char Character){
 
 	return false;
 }
+
+
+int PrecedenceOperator(char Operator){
+	int precedence = -1;
+	switch(Operator){
+    case '(':	precedence = 4;
+        break;
+	case '+':
+	case '-':	precedence = 2;
+		break;
+	case '*':
+	case '/':	precedence = 3;
+		break;
+    case ')':	precedence = 1;
+        break;
+	}
+	return precedence;
+}
